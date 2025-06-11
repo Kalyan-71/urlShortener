c213c8f3374c2cd42f721de29a2250bad5107802
kalyan kumar <pothugantikalyankumar71@gmail.com>
2025-06-11 21:55:05 +0530

    updated frontend axiosinstance for deploy

--- FRONTEND/src/utils/axiosInstance.js
+++ FRONTEND/src/utils/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:3000",
+    // baseURL:"http://localhost:3000",
+    baseURL:"https://url-shortener-backend-dr99.onrender.com",
+    
     timeout:10000, //10s
     withCredentials:true
 })
